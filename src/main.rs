be97990b439eeeec27ada3753c9fd6ff8d5d681b
Kyle Brown <kyleb@liquidrocketry.com>
2021-01-11 18:59:05 -0500

    Basic clap setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
+
+use std::path::PathBuf;
+
+extern crate clap;
+use clap::Clap;
+
 pub mod ast;
 pub mod lexer;
 pub mod parser;
 
+#[derive(Clap, Debug)]
+struct Arguments {
+    #[clap(subcommand)]
+    command: Command
+}
+
+#[derive(Clap, Debug)]
+enum Command {
+    Compile {
+        #[clap(long)]
+        input_path: PathBuf
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    match args.command {
+        Command::Compile { input_path } => {
+            println!("Compiling {:?}", input_path);
+        }
+    }
 }
